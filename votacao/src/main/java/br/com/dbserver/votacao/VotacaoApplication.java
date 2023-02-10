1fc261d38865028e83633b102a6ca3377779a32d
anderson fuhr souza <andersonfuhr@yahoo.com.br>
2023-02-10 17:49:25 -0300

    start do projeto

---
+++ votacao/src/main/java/br/com/dbserver/votacao/VotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoApplication.class, args);
+	}
+
+}
---
+++ votacao/src/test/java/br/com/dbserver/votacao/VotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
