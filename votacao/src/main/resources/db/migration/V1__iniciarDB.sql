356516026b74109b99a4a1b08df5284d6f2e9fac
fuhr-br <andersonfuhr@yahoo.com.br>
2023-02-13 16:27:11 -0300

    adicionado flyway

---
+++ votacao/src/main/resources/db/migration/V1__iniciarDB.sql
@@ -0,0 +1,41 @@
+CREATE TABLE associado (
+ id serial PRIMARY KEY,
+ nome VARCHAR(255) NOT NULL,
+ cpf VARCHAR(255) NOT NULL,
+ status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE assembleia (
+ id serial PRIMARY KEY,
+ inicio timestamp with time zone NOT NULL,
+ fim timestamp with time zone NOT NULL
+);
+
+CREATE TABLE pauta (
+id serial PRIMARY KEY,
+descricao text NOT NULL,
+inicio timestamp with time zone NOT NULL,
+fim timestamp with time zone NOT NULL,
+status text NOT NULL
+);
+
+CREATE TABLE voto (
+ id serial PRIMARY KEY,
+ pauta_id integer NOT NULL,
+ associado_id integer NOT NULL,
+ valor VARCHAR(255) NOT NULL,
+ FOREIGN KEY (pauta_id) REFERENCES pauta (id),
+ FOREIGN KEY (associado_id) REFERENCES associado (id)
+);
+
+CREATE TABLE assembleia_pauta (
+ assembleia_id bigint NOT NULL REFERENCES assembleia (id) ON DELETE CASCADE,
+ pauta_id bigint NOT NULL REFERENCES pauta (id) ON DELETE CASCADE,
+ PRIMARY KEY (assembleia_id, pauta_id)
+);
+
+CREATE TABLE pauta_votacao (
+ pauta_id bigint NOT NULL REFERENCES pauta (id) ON DELETE CASCADE,
+ votos_id bigint NOT NULL REFERENCES voto (id) ON DELETE CASCADE,
+ PRIMARY KEY (pauta_id, votos_id)
+);
