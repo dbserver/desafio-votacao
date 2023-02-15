d9f1c372d1935f1e750a6685ced9d2d9707c8911
Guilherme Moreira <guilhermemoreira55@gmail.com>
2023-02-15 14:34:55 -0300

    add relacionamento pauta e sessao

--- src/main/java/br/tec/db/desafio/business/service/implementation/SessaoServiceImpl.java
+++ src/main/java/br/tec/db/desafio/business/service/implementation/SessaoServiceImpl.java
@@ -1,13 +1,13 @@
 package br.tec.db.desafio.business.service.implementation;
 
-import br.tec.db.desafio.api.v1.dto.pauta.PautaMapperV1;
 import br.tec.db.desafio.api.v1.dto.sessao.SessaoMapperV1;
 import br.tec.db.desafio.api.v1.dto.sessao.SessaoRequestV1;
 import br.tec.db.desafio.api.v1.dto.sessao.SessaoResponseV1;
+import br.tec.db.desafio.business.entity.Pauta;
 import br.tec.db.desafio.business.entity.Sessao;
-import br.tec.db.desafio.business.service.PautaService;
 import br.tec.db.desafio.business.service.SessaoService;
 import br.tec.db.desafio.business.service.implementation.validacao.sessao.ValidacaoSessao;
+import br.tec.db.desafio.repository.PautaRepository;
 import br.tec.db.desafio.repository.SessaoRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +16,12 @@ import java.util.List;
 @Service
 public class SessaoServiceImpl implements SessaoService {
     private final SessaoRepository sessaoRepository;
+    private final PautaRepository pautaRepository;
     private final List<ValidacaoSessao> validacoesSessao;
 
-    public SessaoServiceImpl(SessaoRepository sessaoRepository, List<ValidacaoSessao> validacoesSessao) {
+    public SessaoServiceImpl(SessaoRepository sessaoRepository, PautaRepository pautaRepository, List<ValidacaoSessao> validacoesSessao) {
         this.sessaoRepository = sessaoRepository;
+        this.pautaRepository = pautaRepository;
         this.validacoesSessao = validacoesSessao;
     }
 
@@ -30,6 +32,10 @@ public class SessaoServiceImpl implements SessaoService {
         Sessao sessaoToCreate = SessaoMapperV1.sessaoRequestV1ToSessao(
                 sessaoRequestV1
         );
+        Pauta pautaEncontrada = pautaRepository.findPautaByAssunto
+                (sessaoToCreate.getPauta().getAssunto());
+
+        sessaoToCreate.setPauta(pautaEncontrada);
 
         return SessaoMapperV1.sessaoToSessaoResponseV1(
                 sessaoRepository.save(sessaoToCreate)
--- src/main/java/br/tec/db/desafio/repository/SessaoRepository.java
+++ src/main/java/br/tec/db/desafio/repository/SessaoRepository.java
@@ -3,10 +3,15 @@ package br.tec.db.desafio.repository;
 import br.tec.db.desafio.business.entity.Pauta;
 import br.tec.db.desafio.business.entity.Sessao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SessaoRepository extends JpaRepository<Sessao, Long> {
-    Sessao findSessaoByPauta(@Param("pauta") Pauta pauta);
+//    @Query(value = "SELECT *\n" +
+//            "FROM VOTACAO.SESSAO\n" +
+//            "WHERE assunto =:assuntoPauta",
+//            nativeQuery = true)
+//    Sessao findByAssuntoPauta(@Param("assuntoPauta") String assuntoPauta);
 }
