c4891cdd1d2fc9864af590e2fc004f3884cd2eec
Roger Foss <rogerfsiqueira@gmail.com>
2023-02-24 09:46:14 -0300

    criando SessaoDeVotacaoControllerTest

---
+++ votacao/src/test/java/br/tec/db/votacao/controller/SessaoDeVotacaoControllerTest.java
@@ -0,0 +1,140 @@
+package br.tec.db.votacao.controller;
+
+//path to test file: src\test\java\br\tec\db\votacao\controller\SessaoDeVotacaoControllerTest.java based on PautaControllerTest.java
+
+import br.tec.db.votacao.dto.SessaoDeVotacaoDTO;
+import br.tec.db.votacao.service.SessaoDeVotacaoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SessaoDeVotacaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SessaoDeVotacaoService sessaoDeVotacaoService;
+
+    @Test
+    public void deveCriarUmaSessaoDeVotacaoEmUmaPauta() throws Exception {
+        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L);
+        when(sessaoDeVotacaoService.criarSessaoDeVotacao(any(SessaoDeVotacaoDTO.class))).thenReturn(sessaoDeVotacaoDTO);
+
+        mockMvc.perform(post("/sessao-de-votacao")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inicio\": \"2023-06-01T10:00:00\", \"idPauta\": \"1\"}"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void deveRetornarBadRequestAoCriarSessaoDeVotacaoComDTONull() throws Exception {
+        when(sessaoDeVotacaoService.criarSessaoDeVotacao(any(SessaoDeVotacaoDTO.class))).thenReturn(null);
+
+        mockMvc.perform(post("/sessao-de-votacao")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inicio\": \"null\", \"idPauta\": \"null\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void deveBuscarTodasAsSessoesDeVotacao() throws Exception {
+        List<SessaoDeVotacaoDTO> sessaoDeVotacaoDTO = new ArrayList<>();
+        sessaoDeVotacaoDTO.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L));
+        sessaoDeVotacaoDTO.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 2L));
+        sessaoDeVotacaoDTO.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 3L));
+        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessaoDeVotacaoDTO);
+
+        mockMvc.perform(get("/sessao-de-votacao")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)));
+    }
+
+    @Test
+    public void deveRetornarNotFoundAoBuscarTodasAsSessoesDeVotacaoVazio() throws Exception {
+        List<SessaoDeVotacaoDTO> sessaoDeVotacaoDTO = new ArrayList<>();
+        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessaoDeVotacaoDTO);
+
+        mockMvc.perform(get("/sessao-de-votacao")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    //buscarSessaoDeVotacaoPorId
+    @Test
+    public void deveBuscarSessaoDeVotacaoPorId() throws Exception {
+        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = new SessaoDeVotacaoDTO(LocalDateTime.now(), 3L);
+        when(sessaoDeVotacaoService.buscarSessaoDeVotacaoPorId(1L)).thenReturn(sessaoDeVotacaoDTO);
+
+        mockMvc.perform(get("/sessao-de-votacao/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deveRetornarNotFoundAoBuscarSessaoDeVotacaoPorIdInexistente() throws Exception {
+
+        mockMvc.perform(get("/sessao-de-votacao/10")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deveRetornarBadRequestAoBuscarSessaoDeVotacaoPorIdInvalido() throws Exception {
+
+        mockMvc.perform(get("/sessao-de-votacao/abc")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void deveBuscarSessaoDeVotacaoPorPauta() throws Exception {
+        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L);
+        when(sessaoDeVotacaoService.buscarSessaoDeVotacaoPorPauta(1L)).thenReturn(sessaoDeVotacaoDTO);
+
+        mockMvc.perform(get("/sessao-de-votacao/pauta/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deveRetornarNotFoundAoBuscarSessaoDeVotacaoPorPautaInexistente() throws Exception {
+
+        mockMvc.perform(get("/sessao-de-votacao/pauta/10")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deveRetornarBadRequestAoBuscarSessaoDeVotacaoPorPautaInvalido() throws Exception {
+
+        mockMvc.perform(get("/sessao-de-votacao/pauta/abc")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+}
+
+
+
