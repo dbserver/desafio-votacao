c177ca895fde8a15b98e29de1fd40fa6ce16dde9
grego.rogerio <grego.rogerio@gmail.com>
2023-02-24 22:13:44 -0300

    Estrutura Inicial do projeto

---
+++ src/main/java/com/dbserver/desafio/votacao/Application.java
@@ -0,0 +1,13 @@
+package com.dbserver.desafio.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
