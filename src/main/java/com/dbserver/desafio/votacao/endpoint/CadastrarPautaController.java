48f077db81e867c2ddd00f8397a024853d7e8303
grego.rogerio <grego.rogerio@gmail.com>
2023-02-28 13:08:27 -0300

    Criacao do Swagger da API

--- src/main/java/com/dbserver/desafio/votacao/endpoint/CadastrarPautaController.java
+++ src/main/java/com/dbserver/desafio/votacao/endpoint/CadastrarPautaController.java
@@ -4,9 +4,11 @@ import com.dbserver.desafio.votacao.endpoint.constant.EndpointURL;
 import com.dbserver.desafio.votacao.endpoint.dto.PautaDTO;
 import com.dbserver.desafio.votacao.endpoint.mapper.PautaDtoParaPautaMapper;
 import com.dbserver.desafio.votacao.endpoint.mapper.PautaParaPautaDtoMapper;
+import com.dbserver.desafio.votacao.endpoint.swagger.CadastrarPautaControllerSwagger;
 import com.dbserver.desafio.votacao.usecase.domain.Pauta;
 import com.dbserver.desafio.votacao.usecase.pauta.SalvarPautaUsecase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +16,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-public class CadastrarPautaController {
+public class CadastrarPautaController implements CadastrarPautaControllerSwagger {
 
     private final SalvarPautaUsecase salvarPautaUsecase;
 
     @PostMapping(value = EndpointURL.CADASTRAR_PAUTA_URL)
     public ResponseEntity<PautaDTO> cadastrarPauta(@RequestBody PautaDTO pautaDTO) {
+
+        log.info("[CadastrarPautaController] Requisição HTTP POST recebida.");
 
         Pauta pauta = PautaDtoParaPautaMapper.INSTANCE.map(pautaDTO);
 
