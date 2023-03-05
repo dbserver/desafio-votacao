d33d8a60bcd02bc32ab44885786c6c3c944a4cde
Cássio Ribeiro Pereira <cassio@gomining.com.br>
2023-03-05 12:00:48 -0300

    feat: start project

---
+++ src/main/java/com/dbserver/DesafioVotosApplication.java
@@ -0,0 +1,13 @@
+package com.dbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotosApplication.class, args);
+	}
+
+}
