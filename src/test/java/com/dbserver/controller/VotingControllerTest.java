921e2e4ee9296a969e5a071e3bf1fb09f2bfcd4d
Cássio Ribeiro Pereira <cassio@gomining.com.br>
2023-03-11 18:51:00 -0300

    test: implement integration test of voting

---
+++ src/test/java/com/dbserver/controller/VotingControllerTest.java
@@ -0,0 +1,118 @@
+package com.dbserver.controller;
+
+import com.dbserver.model.dto.AgendaCreateDTO;
+import com.dbserver.model.dto.VotingCreateDTO;
+import com.dbserver.model.entity.Agenda;
+import com.dbserver.model.entity.Voting;
+import com.dbserver.repository.AgendaRepository;
+import com.dbserver.repository.VoteRepository;
+import com.dbserver.repository.VotingRepository;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.internal.matchers.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(locations = "classpath:test.properties")
+public class VotingControllerTest {
+
+    @Autowired
+    private AgendaRepository agendaRepository;
+    @Autowired
+    private VotingRepository votingRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @LocalServerPort
+    private int port;
+
+    private String URL;
+
+    private RequestSpecification requestSpec;
+
+    @BeforeAll
+    public void init() {
+        URL = "http://localhost:" + port + "/api/v1/voting";
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(URL)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    @Test
+    public void shouldCreateVotingAndStatus201() {
+        Agenda agenda = agendaRepository.save(Agenda.builder().build());
+        VotingCreateDTO votingCreateDTO = VotingCreateDTO.builder()
+                .idAgenda(agenda.getId())
+                .duration(6000l)
+                .build();
+        given(requestSpec)
+                .body(votingCreateDTO)
+                .post()
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("idAgenda", equalTo(agenda.getId()));
+    }
+
+    @Test
+    public void shouldThrowBadRequestExceptionWithoutRequiredFieldAndStatus400() {
+        VotingCreateDTO votingCreateDTO = VotingCreateDTO.builder()
+                .idAgenda("idAgenda")
+                .duration(null)
+                .build();
+        given(requestSpec)
+                .body(votingCreateDTO)
+                .post()
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void shouldThrowNotFoundExceptionWithNonExistentAgendaAndStatus404() {
+        VotingCreateDTO votingCreateDTO = VotingCreateDTO.builder()
+                .idAgenda("idAgenda")
+                .duration(6000l)
+                .build();
+        given(requestSpec)
+                .body(votingCreateDTO)
+                .post()
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("Agenda not found"));
+    }
+
+    @Test
+    void shouldGetVotingById() {
+        Voting voting = votingRepository.save(Voting.builder().idAgenda(UUID.randomUUID().toString()).build());
+        given(requestSpec)
+                .get("/" + voting.getId())
+                .then()
+                .statusCode(200)
+                .body("id", notNullValue());
+    }
+
+    @Test
+    void shouldThrowNotFoundExceptionWithNonexistentVoting() {
+        given(requestSpec)
+                .get("/" + UUID.randomUUID())
+                .then()
+                .statusCode(404)
+                .body("message", containsString("Voting not found"));
+    }
+
+}
