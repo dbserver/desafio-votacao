19fa525495ace8f78325ea09c5f3fce29981e75a
Cássio Ribeiro Pereira <cassio@gomining.com.br>
2023-03-14 10:45:50 -0300

    refactor: add exception handling in mappers

--- src/main/java/com/dbserver/model/mapper/AgendaMapper.java
+++ src/main/java/com/dbserver/model/mapper/AgendaMapper.java
@@ -1,24 +1,39 @@
 package com.dbserver.model.mapper;
 
+import com.dbserver.exception.BusinessException;
 import com.dbserver.model.dto.AgendaCreateDTO;
 import com.dbserver.model.dto.AgendaDTO;
 import com.dbserver.model.entity.Agenda;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AgendaMapper {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ObjectMapper objectMapper;
 
     public Agenda toEntity(AgendaCreateDTO agendaCreateDTO) {
-        return objectMapper.convertValue(agendaCreateDTO, Agenda.class);
+        try {
+            return objectMapper.convertValue(agendaCreateDTO, Agenda.class);
+        } catch (RuntimeException e) {
+            logger.error("Error mapping agenda", e);
+            throw new BusinessException();
+        }
     }
 
     public AgendaDTO toDTO(Agenda agenda) {
-        return objectMapper.convertValue(agenda, AgendaDTO.class);
+        try {
+            return objectMapper.convertValue(agenda, AgendaDTO.class);
+        } catch (RuntimeException e) {
+            logger.error("Error mapping agenda", e);
+            throw new BusinessException();
+        }
     }
 
 }
--- src/main/java/com/dbserver/model/mapper/VotingSessionMapper.java
+++ src/main/java/com/dbserver/model/mapper/VotingSessionMapper.java
@@ -1,9 +1,12 @@
 package com.dbserver.model.mapper;
 
+import com.dbserver.exception.BusinessException;
 import com.dbserver.model.dto.VotingSessionCreateDTO;
 import com.dbserver.model.dto.VotingSessionDTO;
 import com.dbserver.model.entity.VotingSession;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,20 +17,32 @@ import java.util.Optional;
 @Component
 public class VotingSessionMapper {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ObjectMapper objectMapper;
 
     public VotingSession toEntity(VotingSessionCreateDTO votingSessionCreateDTO) {
-        VotingSession voting = objectMapper.convertValue(votingSessionCreateDTO, VotingSession.class);
-        Long duration = Optional.ofNullable(voting.getDuration()).orElse(60000L);
-        voting.setDuration(duration);
-        voting.setStartDate(LocalDateTime.now());
-        voting.setEndDate(LocalDateTime.now().plus(duration, ChronoUnit.MILLIS));
-        return voting;
+        try {
+            VotingSession voting = objectMapper.convertValue(votingSessionCreateDTO, VotingSession.class);
+            Long duration = Optional.ofNullable(voting.getDuration()).orElse(60000L);
+            voting.setDuration(duration);
+            voting.setStartDate(LocalDateTime.now());
+            voting.setEndDate(LocalDateTime.now().plus(duration, ChronoUnit.MILLIS));
+            return voting;
+        } catch (RuntimeException e) {
+            logger.error("Error mapping voting session", e);
+            throw new BusinessException();
+        }
     }
 
     public VotingSessionDTO toDTO(VotingSession voting) {
-        return objectMapper.convertValue(voting, VotingSessionDTO.class);
+        try {
+            return objectMapper.convertValue(voting, VotingSessionDTO.class);
+        } catch (RuntimeException e) {
+            logger.error("Error mapping voting session", e);
+            throw new BusinessException();
+        }
     }
 
 }
