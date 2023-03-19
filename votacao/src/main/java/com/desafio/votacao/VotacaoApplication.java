d6ca163f867bc412a3a53886b415218874b047ef
PabloTs <pablo.santos@globalweb.com.br>
2023-03-19 20:01:07 -0300

    projeto teste votacao

---
+++ votacao/src/main/java/com/desafio/votacao/VotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoApplication.class, args);
+	}
+
+}
