b9805bcc8b0f3847de69e7a5fa096fd81b3a6a7f
Rayane Paiva <rayane.paiva@dbserver.com.br>
2023-04-12 12:32:35 -0300

    refactor: removida classe Resultado desnecessaria

--- src/main/java/db/desafiovotacao/dto/ResultadoResponse.java
+++ src/main/java/db/desafiovotacao/dto/ResultadoResponse.java
@@ -1,12 +1,6 @@
 package db.desafiovotacao.dto;
 
-import db.desafiovotacao.model.Resultado;
-
 public record ResultadoResponse(
         Integer votosPositivos,
         Integer votosNegativos
-) {
-    public ResultadoResponse(Resultado resultado){
-        this(resultado.getVotosPositivos(), resultado.getVotosNegativos());
-    }
-}
+) {}
