ebb41f45143e02829affe259a2477b17765aad30
Jullie Paixão <jlpaixaof@gmail.com>
2023-08-06 10:52:54 -0300

    Iniciando projeto com Spring Initializr

---
+++ src/main/java/com/sicredi/cooperativismo/CooperativismoApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.cooperativismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativismoApplication.class, args);
+	}
+
+}
