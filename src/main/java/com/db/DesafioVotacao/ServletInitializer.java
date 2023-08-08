0205d8ad59dc6007197a7eb32ff65b1bb5181699
felipe feyh <felipefeyh2@gmail.com>
2023-08-07 22:17:17 -0300

    build(1) - Configurações Iniciais

---
+++ src/main/java/com/db/DesafioVotacao/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.db.DesafioVotacao;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DesafioVotacaoApplication.class);
+	}
+
+}
