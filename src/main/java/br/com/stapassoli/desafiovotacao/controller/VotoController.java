1dbbc3994317aadfcb6b2a1d90846fbc13a34a74
hugoodesa <hugoodesa@gmail.com>
2023-08-25 02:05:55 -0300

    criado voto controller , service e repository e também DTO da entity sessao

---
+++ src/main/java/br/com/stapassoli/desafiovotacao/controller/VotoController.java
@@ -0,0 +1,24 @@
+package br.com.stapassoli.desafiovotacao.controller;
+
+import br.com.stapassoli.desafiovotacao.dto.SessaoDTO;
+import br.com.stapassoli.desafiovotacao.entity.Sessao;
+import br.com.stapassoli.desafiovotacao.service.SessaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sessao")
+@RequiredArgsConstructor
+public class VotoController {
+
+    private final SessaoService sessaoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Sessao> cadastrarSessao(@RequestBody SessaoDTO sessaoDTO) {
+        return sessaoService.cadastrarSessao(sessaoDTO);
+    }
+
+}
