2f5df405e72a8453718d1db985b09491192d6c47
Vinicius <vgs.aguirre@gmail.com>
2023-09-18 02:28:53 -0300

    Ajutado AssociadoValidator(Retirado a validacao de cpf e nome, agora será realizado pelas annotations do spring no AssociadoResquestDto)

--- src/main/java/com/desafio/projeto_votacao/utils/AssociadoValidator.java
+++ src/main/java/com/desafio/projeto_votacao/utils/AssociadoValidator.java
@@ -2,7 +2,6 @@ package com.desafio.projeto_votacao.utils;
 
 import com.desafio.projeto_votacao.repository.AssociadoRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,16 +9,6 @@ import org.springframework.stereotype.Component;
 public class AssociadoValidator {
     
     private final AssociadoRepository associadoRepository;
-
-/*
-    public boolean nomeVazioOuNulo(String nome) {
-        return Strings.isEmpty(nome);
-    }
-*/
-
-/*    public boolean cpfVazioOuNulo(String cpf) {
-        return Strings.isEmpty(cpf);
-    }*/
 
     public String removerMascaraCPF(String cpf) {
         return cpf.replaceAll("\\D", "");
