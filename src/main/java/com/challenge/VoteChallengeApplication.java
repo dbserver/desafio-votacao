46284ef7eb291d3d6c51d3d8a5c21665f6e13d37
tfoliveira <tfoliveira11@gmail.com>
2023-10-10 12:43:19 -0300

    feat: init application

---
+++ src/main/java/com/challenge/VoteChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteChallengeApplication.class, args);
+	}
+
+}
