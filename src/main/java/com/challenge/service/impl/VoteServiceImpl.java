32adf9efdbf04b6321445dc1805f371bc59fb5bf
tfoliveira <tfoliveira11@gmail.com>
2023-10-10 12:48:49 -0300

    feat: implements vote service

---
+++ src/main/java/com/challenge/service/impl/VoteServiceImpl.java
@@ -0,0 +1,49 @@
+package com.challenge.service.impl;
+
+import com.challenge.dto.VoteRequestDto;
+import com.challenge.model.Associate;
+import com.challenge.model.StaveSession;
+import com.challenge.model.Vote;
+import com.challenge.repository.AssociateRepository;
+import com.challenge.repository.StaveSessionRepository;
+import com.challenge.repository.VoteRepository;
+import com.challenge.service.VoteService;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import static com.challenge.enums.SessionStatusEnum.CLOSE;
+
+@Service
+@AllArgsConstructor
+public class VoteServiceImpl implements VoteService {
+
+    private final Logger logger = LoggerFactory.getLogger(VoteServiceImpl.class);
+    private final VoteRepository voteRepository;
+    private final StaveSessionRepository staveSessionRepository;
+    private final AssociateRepository associateRepository;
+
+    @Override
+    public Vote save(VoteRequestDto request) {
+        final Associate associate = associateRepository.getReferenceById(request.getAssociateId());
+
+        if (voteRepository.existsByAssociateAndSession(request.getAssociateId(), request.getStaveSessionId())) {
+            throw new IllegalArgumentException("Associado já votou para essa sessão.");
+        }
+
+        final StaveSession staveSession = staveSessionRepository.getReferenceById(request.getStaveSessionId());
+
+        if (CLOSE == staveSession.getStatus()) {
+            throw new IllegalArgumentException("Sessão fechada para voto");
+        }
+
+        Vote vote = Vote.builder()
+                .vote(request.getVote())
+                .associate(associate)
+                .staveSession(staveSession)
+                .build();
+
+        return voteRepository.save(vote);
+    }
+}
