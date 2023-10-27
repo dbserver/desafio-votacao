433b140cc23f600302dd924a544e156dccde7c86
Fernando <fernandofritzen9@gmail.com>
2023-10-27 12:52:26 -0300

    Project created

---
+++ backend/src/test/java/com/voting/MainTests.java
@@ -0,0 +1,13 @@
+package com.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
