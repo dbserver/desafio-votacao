f4bcb8bc9f80fd323a3bf8405fcd6e73a27edead
Fernando <fernandofritzen9@gmail.com>
2023-11-02 20:13:27 -0300

    Added session service

---
+++ backend/src/main/java/com/voting/services/SessionService.java
@@ -0,0 +1,38 @@
+package com.voting.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.voting.entities.Session;
+import com.voting.entities.Topic;
+import com.voting.repositories.SessionRepository;
+
+@Service
+public class SessionService {
+
+	@Autowired
+	private SessionRepository sessionRepository;
+
+	@Autowired
+	private TopicService topicService;
+
+	public void startVotingSession(Integer topicId, Integer minutesVoting) {
+		Topic topic = topicService.findById(topicId);
+
+		createSession(topic, closingDate(minutesVoting));
+	}
+
+	private void createSession(Topic topic, Instant closingDate) {
+		Session session = new Session(Instant.now(), closingDate, topic);
+
+		sessionRepository.save(session);
+	}
+
+	private Instant closingDate(Integer minutesVoting) {
+		return minutesVoting != null && minutesVoting > 0 ? Instant.now().plusSeconds(minutesVoting * 60)
+				: Instant.now().plusSeconds(60);
+	}
+
+}
