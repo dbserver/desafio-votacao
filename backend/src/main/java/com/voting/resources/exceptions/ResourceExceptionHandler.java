061599b52b2064ff2495fe87733ddff8b40105c4
Fernando <fernandofritzen9@gmail.com>
2023-11-03 01:19:07 -0300

    Added bad request exception

--- backend/src/main/java/com/voting/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/voting/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.voting.services.exceptions.BadRequestException;
 import com.voting.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,16 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<StandardError> badRequest(BadRequestException e, HttpServletRequest request) {
+		String error = "Bad request";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 				request.getRequestURI());
 
