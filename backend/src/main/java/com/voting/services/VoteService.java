be9622849e50aea3d2403d62042ef0fdc3ed24db
Fernando <fernandofritzen9@gmail.com>
2023-11-03 01:21:53 -0300

    Added vote validations

--- backend/src/main/java/com/voting/services/VoteService.java
+++ backend/src/main/java/com/voting/services/VoteService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.voting.entities.Session;
 import com.voting.entities.Vote;
 import com.voting.repositories.VoteRepository;
+import com.voting.services.exceptions.BadRequestException;
+import com.voting.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class VoteService {
@@ -22,10 +24,20 @@ public class VoteService {
 	private SessionService sessionService;
 
 	public void vote(Integer topicId, Vote vote) {
-		Session session = sessionService.findByTopic(topicService.findById(topicId));
+		Session session = sessionService.findByTopic(topicService.findById(topicId))
+				.orElseThrow(() -> new ResourceNotFoundException("A sessão da votação não foi encontrada!"));
+
+		if (Instant.now().isAfter(session.getClosingDate())) {
+			throw new BadRequestException("A sessão já foi encerrada!");
+		}
 
 		vote.setSession(session);
 		vote.setMoment(Instant.now());
+
+		if (voteRepository.existsByVotingSessionAndCpfVoter(session, vote.getCpfVoter())) {
+			throw new BadRequestException(
+					"Você já registrou o seu voto para esta pauta, é permitido apenas um voto por pessoa!");
+		}
 
 		voteRepository.save(vote);
 	}
