6407784dd1cb3e3210d3b9183a3d56a9dd40f75e
Fernando <fernandofritzen9@gmail.com>
2023-11-03 01:20:32 -0300

    Added vote by session and cpf voter

--- backend/src/main/java/com/voting/repositories/VoteRepository.java
+++ backend/src/main/java/com/voting/repositories/VoteRepository.java
@@ -3,8 +3,10 @@ package com.voting.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.voting.entities.Session;
 import com.voting.entities.Vote;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
+	Boolean existsByVotingSessionAndCpfVoter(Session session, String cpfVoter);
 }
