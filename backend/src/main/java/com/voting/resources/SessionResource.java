99a8f7a035d437f69a7fc88e68deb3f783d36bfb
Fernando <fernandofritzen9@gmail.com>
2023-11-06 15:42:26 -0300

    Added session api doc

--- backend/src/main/java/com/voting/resources/SessionResource.java
+++ backend/src/main/java/com/voting/resources/SessionResource.java
@@ -12,18 +12,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.voting.entities.Session;
 import com.voting.services.SessionService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping(value = "/v1/sessions")
 public class SessionResource {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(SessionResource.class);
 
 	@Autowired
 	SessionService sessionService;
 
+	@Operation(summary = "Busca as sessões", method = "GET")
 	@GetMapping
 	public ResponseEntity<List<Session>> findAll(@RequestParam String showClosedSessions) {
 		logger.info("Consultando sessões...");
@@ -32,6 +36,7 @@ public class SessionResource {
 		return ResponseEntity.ok().body(list);
 	}
 
+	@Operation(summary = "Inicia uma sessão de votação", method = "POST")
 	@PostMapping("/{topicId}/start-voting-session")
 	public ResponseEntity startVotingSession(@PathVariable("topicId") Integer topicId,
 			@RequestParam Integer minutesVoting) {
