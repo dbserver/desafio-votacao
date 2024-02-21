63e3ac979715c8c23afd0d3c3c9b74b69a517c35
Claudineia Fernandes <fernandesclaudineia83@gmail.com>
2024-02-21 18:37:59 -0300

    Commit inicial

---
+++ src/test/java/com/fernandesclaudi/desafiovotacao/DesafioVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandesclaudi.desafiovotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
