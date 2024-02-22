341d8d9615c12d62b8fda673723810e478aeadb5
Claudineia Fernandes <fernandesclaudineia83@gmail.com>
2024-02-21 22:46:42 -0300

    fix: Utilizando variável para centralizar versão da API

--- src/main/java/com/fernandesclaudi/desafiovotacao/controller/AssociadoController.java
+++ src/main/java/com/fernandesclaudi/desafiovotacao/controller/AssociadoController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/associado")
+@RequestMapping("${VersionApi.VERSION}/associado")
 public class AssociadoController {
     @Autowired
 
