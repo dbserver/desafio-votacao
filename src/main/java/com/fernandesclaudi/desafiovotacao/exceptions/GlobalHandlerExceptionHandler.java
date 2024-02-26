eb2624af0aa6bc105544ccb9370159cc6e89224b
Claudineia Fernandes <fernandesclaudineia83@gmail.com>
2024-02-25 23:18:54 -0300

    feat: Melhorando as validações dos valores esperados. Centralizandoas exceções no handler global.

--- src/main/java/com/fernandesclaudi/desafiovotacao/exceptions/GlobalHandlerExceptionHandler.java
+++ src/main/java/com/fernandesclaudi/desafiovotacao/exceptions/GlobalHandlerExceptionHandler.java
@@ -1,16 +1,49 @@
 package com.fernandesclaudi.desafiovotacao.exceptions;
 
-import org.springframework.http.ResponseEntity;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalHandlerExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(IBaseException.class)
-    protected ResponseEntity<Object> handleConflict(IBaseException ex, WebRequest request) {
-        return ResponseEntity.status(ex.getHttpStatus()).body(ex.getMessage());
+    @Override protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, @NotNull HttpHeaders headers, @NotNull HttpStatusCode status,
+            @NotNull WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleHandlerMethodValidationException(HandlerMethodValidationException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity.status(status).body(ex.getAllValidationResults().stream().map(validationResult -> {
+            String errorMessage = validationResult.getResolvableErrors().stream().findFirst().get().getDefaultMessage();
+            assert errorMessage != null;
+            return errorMessage;
+        }));
+    }
+
+    @ExceptionHandler(IBaseException.class)
+    public ResponseEntity<Object> handleConflict(IBaseException ex, WebRequest request) {
+       return ResponseEntity.status(ex.getHttpStatus()).body(ex.getMessage());
+    }
+
+
 }
